cc0fbcbefc1894ba9d3908964e57e5b275b1e2f2
tsamp1kos <t8220114@aueb.gr>
2024-11-24 15:42:29 +0200

    DB

---
+++ plantsDB.java
@@ -0,0 +1,20 @@
+public class plantsDB {
+    //Plants Names
+    public static final String[] plantsNames = {
+        "Apples", "Oranges", "Peaches", "Pears", "Grapes",
+        "Kiwis", "Olives", "Figs", "Pomegranates", "Lemons"
+    };
+    //Plants Fixed Data
+    public static final double[][] plantsFixedData = {
+        {50, 4, 50},  // Apples
+        {60, 5, 70},  // Oranges
+        {45, 4.5, 60},  // Peaches
+        {50, 4.5, 55},  // Pears
+        {70, 1.5, 40},  // Grapes
+        {80, 3, 80},  // Kiwis
+        {30, 8, 20},  // Olives
+        {40, 6, 30},  // Figs
+        {35, 5.5, 40},  // Pomegranates
+        {55, 4, 60}   // Lemons
+    };
+}
