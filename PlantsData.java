d44ccc3035294aec4fcf167211f5e4bdd4d697de
Maria <126980052+MariaMal1@users.noreply.github.com>
2025-01-11 13:34:05 +0200

    Add files via upload

---
+++ PlantsData.java
@@ -0,0 +1,39 @@
+
+public class PlantsData {
+    private int area;
+    private double laborCost;
+    private int availableIrrigation;
+
+    // Constructor
+    public PlantsData(int area, double laborCost, int availableIrrigation) {
+        this.area = area;
+        this.laborCost = laborCost;
+        this.availableIrrigation = availableIrrigation;
+    }
+
+    // Getters
+    public int getArea() {
+        return area;
+    }
+
+    public double getLaborCost() {
+        return laborCost;
+    }
+
+    public int getAvailableIrrigation() {
+        return availableIrrigation;
+    }
+
+    // Setters
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public void setLaborCost(double laborCost) { // Εδώ αλλάξαμε σε double
+        this.laborCost = laborCost;
+    }
+
+    public void setAvailableIrrigation(int availableIrrigation) {
+        this.availableIrrigation = availableIrrigation;
+    }
+}
